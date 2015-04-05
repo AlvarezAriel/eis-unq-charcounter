35487c960b94330b489ee5eb058f990602a8a74f
ariel <alvarez.ariel.work@gmail.com>
2015-04-05 17:05:41 -0300

    Agregado comentario a la solcuión

--- model/char_counter.rb
+++ model/char_counter.rb
@@ -1,4 +1,10 @@
 class CharCounter
+
+  # La interfaz que se pide del diccionario no me parece adecuada
+  # Se supone que es un diccionario que me dice cuántas veces apareció un char, con lo cual, si pregunto por un
+  # char que no estaba, debería devolver 0, que es exactamente la cantidad de veces que apareció, no nil!
+  # Porque nil ni siquiera es del mismo tipo!
+
   def CharCounter.count(str)
     hash = if str.nil?
         Hash.new;
