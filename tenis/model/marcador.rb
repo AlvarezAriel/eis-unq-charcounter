dbbadf57f0a9133c562966c5f9721cc8098706a3
ariel <alvarez.ariel.work@gmail.com>
2015-04-27 04:27:29 -0300

    Marcador debe comenzar con games en (0,0)

--- tenis/model/marcador.rb
+++ tenis/model/marcador.rb
@@ -1,7 +1,9 @@
 class Marcador
-  attr_accessor :set
+  attr_reader :set, :game, :puntos
 
   def initialize
-    @set = [0,0]
+    (@set,@game,@puntos) = [[0,0],[0,0],[0,0]]
   end
+
+
 end
--- tenis/spec/marcador_spec.rb
+++ tenis/spec/marcador_spec.rb
@@ -3,10 +3,20 @@ require_relative '../model/marcador.rb'
 
 describe 'Marcador' do
 
-  it 'debe comenzar con sets en (0,0)' do
-    (player_one_set, player_two_set) = Marcador.new.set
-    player_one_set.should be 0
-    player_two_set.should be 0
+  describe 'al comenzar el partido' do
+    it 'debe tener sus sets en (0,0)' do
+      (player_one_set, player_two_set) = Marcador.new.set
+      player_one_set.should be 0
+      player_two_set.should be 0
+    end
+
+    it 'debe tener sus games en (0,0)' do
+      (player_one_game, player_two_game) = Marcador.new.game
+      player_one_game.should be 0
+      player_two_game.should be 0
+    end
+
+
   end
 
 end
