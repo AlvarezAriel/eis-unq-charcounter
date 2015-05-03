9d19b0512e4df85a128be16da2eb7ed1ea6980a4
ariel <alvarez.ariel.work@gmail.com>
2015-05-03 01:49:06 -0300

    batalla_naval: los boards pueden tener ships

--- batalla_naval/app/models/Board.rb
+++ batalla_naval/app/models/Board.rb
@@ -1,7 +1,32 @@
 class Board
   attr_reader :size
-  def initialize(x,y)
-    @size = [x,y]
+  def initialize(width,height)
+    @size = [width,height]
+    @ships = []
   end
+
+  def add_ship_at(x,y)
+    new_ship = Ship.new(x,y)
+
+    raise 'Position out of board' unless  new_ship.positions.all? {|(px,py)| is_in_board?(px,py)}
+
+    @ships.push(Ship.new(x,y))
+  end
+
+  def is_in_board?(x,y) x >= 0 && y >= 0 && y < @size[1] && x < @size[0] end
+
+  def is_water_at?(x,y) @ships.none? {|ship| ship.occupies?(x,y)} end
+
+end
+
+# The specified Ship is two positions long and can only be
+# vertically aligned.
+class Ship
+
+  def positions; @positions end
+
+  def initialize(from_x,from_y) @positions = [ [from_x,from_y], [from_x,from_y+1] ] end
+
+  def occupies?(x,y) @positions.any? { |pos| pos == [x,y] } end
 
 end
--- batalla_naval/features/step_definitions/battleship_steps.rb
+++ batalla_naval/features/step_definitions/battleship_steps.rb
@@ -1,13 +1,33 @@
 require_relative '../../app/models/Board.rb'
 
-Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |arg1, arg2|
+Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |width, height|
   @board = Board.new arg1, arg2
 end
 
-Given(/^I create a small ship in position "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I create a small ship in position "([0-9]+?):([0-9]+?)"$/) do |x,y|
+  @board
 end
 
-Then(/^position "(.*?)" is not empty$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^position "([0-9]+?):([0-9]+?)" is not empty$/) do |x,y|
+  @board.is_water_at?(x,y).should be false
+end
+
+And(/^a large ship in position: "([0-9]+?):([0-9]+?)"$/) do |x,y|
+  pending
+end
+
+Given(/^I shoot to position "([0-9]+?):([0-9]+?)"$/) do |x,y|
+  pending
+end
+
+Then(/^I get hit$/) do
+  pending
+end
+
+Then(/^I get water$/) do
+  pending
+end
+
+Then(/^I get sink$/) do
+  pending
 end
