a7239cc7341ea29d386286b1e598b8ad7b6c7a89
ariel <alvarez.ariel.work@gmail.com>
2015-05-03 02:04:30 -0300

    batalla_naval: step - create a small ship in position - is not water

--- batalla_naval/spec/models/battleship_spec.rb
+++ batalla_naval/spec/models/battleship_spec.rb
@@ -16,4 +16,16 @@ describe 'Battleship' do
     expect(@board.size[1]).to eq 5
   end
 
+  describe 'when using a 5x5 board' do
+
+    it 'should have water on 1:1' do
+      expect(@board.is_water_at?(1,1)).to be true
+    end
+
+    it 'should not have water on 1:1 if a ship is in 1:1' do
+      @board.add_ship_at(1,1)
+      expect(@board.is_water_at?(1,1)).to be false
+    end
+
+  end
 end
