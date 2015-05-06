198fbf4b7ad0338a6183ea4c61ad5d8969c7f125
ariel <alvarez.ariel.work@gmail.com>
2015-05-06 15:32:34 -0300

    batalla_naval: [refactor] simplified battleship_steps

--- batalla_naval/features/step_definitions/battleship_steps.rb
+++ batalla_naval/features/step_definitions/battleship_steps.rb
@@ -1,10 +1,10 @@
 require_relative '../../app/models/Board.rb'
 
-Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |width, height|
+Given(/^a board with dimensions "([0-9]+)" x "([0-9]+)"$/) do |width, height|
   @board = Board.new width, height
 end
 
-Given(/^(?:I create )?a (large|small) ship in position "([0-9]+?):([0-9]+?)"$/) do |ship_type, x,y|
+Given(/^(?:I create )?a (large|small) ship in position "([0-9]+):([0-9]+)"$/) do |ship_type, x,y|
   @ship = Ship.send("#{ship_type}_at", x ,y)
   begin
     @board.add_ship @ship
@@ -15,11 +15,6 @@ end
 
 Then(/^position "([0-9]+?):([0-9]+?)" is not empty$/) do |x,y|
   @board.is_water_at?(x,y).should be false
-end
-
-And(/^I create a large ship in position: "([0-9]+?):([0-9]+?)"$/) do |x,y|
-  @ship = Ship.large_at(x, y)
-  @board.add_ship @ship
 end
 
 Given(/^I shoot to position "([0-9]+?):([0-9]+?)"$/) do |x,y|
@@ -39,10 +34,11 @@ Then(/^I get sink$/) do
   @ship.is_sink?.should be true
 end
 
+Then(/^an error should be thrown$/) do
+  @error.should_not be_nil
+end
+
 Transform /^(-?\d+)$/ do |number|
   number.to_i
 end
 
-Then(/^an error should be thrown$/) do
-  @error.should_not be_nil
-end
