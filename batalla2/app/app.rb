1f9edf84e96add1c1357c7ed1dfe683af09d408d
ariel <alvarez.ariel.work@gmail.com>
2015-05-25 10:36:34 -0300

    Agregado service de small_ship

--- batalla2/app/app.rb
+++ batalla2/app/app.rb
@@ -15,30 +15,39 @@ module Battleship
     register Padrino::Sprockets
     sprockets
 
+    set :protect_from_csrf, false
+
     enable :sessions
 
     get '/game' do
-      session[:board] = Board.new(params[:width], params[:height])
+      session[:board] = Board.new(params[:width].to_i, params[:height].to_i)
       render 'batalla/inicio'
     end
 
-    put 'small_ship' do
+    put '/small_ship', :provides => :json do
+      position = JSON.parse request.body.read
+      puts (position[1])
+      b= board
+      b.add_ship(Ship.small_at(position[0].to_i,position[1].to_i))
+      # self.board.add_ship(Ship.small_at(position[0],position[1]))
+
+      content_type :json
+      Oj.dump b
+    end
+
+    put '/long_ship', :provides => :json do
 
     end
 
-    put 'long_ship' do
+    put '/shoot', :provides => :json do
 
     end
 
-    post 'shoot' do
+    post '/board_size', :provides => :json do
 
     end
 
-    post 'board_size' do
-
-    end
-
-    get 'board' do
+    get '/board' do
       content_type :json
       Oj.dump board
     end
--- batalla2/app/assets/javascripts/app/board.coffee
+++ batalla2/app/assets/javascripts/app/board.coffee
@@ -2,8 +2,17 @@ app = angular.module 'batalla2', []
 
 app.controller 'boardCtrl', ($scope, $http) ->
   $scope.boardSize = [0,0]
+
   $scope.rows =    ()->  _.range(0,$scope.boardSize[0]).reverse()
   $scope.columns = ()->  _.range(0,$scope.boardSize[1])
+
+  $scope.action = "shoot"
+
+  $scope.cellClicked = (x,y) ->
+    $http.put $scope.action, [x,y]
+    .success (data) ->
+      console.log(data)
+
 
   $http.get 'board'
     .success (data) ->
