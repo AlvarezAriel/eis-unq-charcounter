99f436201383e18acceaeaf698395fddab4763eb
ariel <alvarez.ariel.work@gmail.com>
2015-05-25 10:47:13 -0300

    Refactor de board action

--- batalla2/app/app.rb
+++ batalla2/app/app.rb
@@ -25,29 +25,25 @@ module Battleship
     end
 
     put '/small_ship', :provides => :json do
-      position = JSON.parse request.body.read
-      puts (position[1])
-      b= board
-      b.add_ship(Ship.small_at(position[0].to_i,position[1].to_i))
-      # self.board.add_ship(Ship.small_at(position[0],position[1]))
-
-      content_type :json
-      Oj.dump b
+      board_action { |x,y| board.add_ship(Ship.small_at(x,y)) }
     end
 
     put '/long_ship', :provides => :json do
-
+      board_action { |x,y| board.add_ship(Ship.large_at(x,y)) }
     end
 
     put '/shoot', :provides => :json do
-
-    end
-
-    post '/board_size', :provides => :json do
-
+      board_action { |x,y| board.shoot_at(x,y) }
     end
 
     get '/board' do
+      content_type :json
+      Oj.dump board
+    end
+
+    def board_action(&block)
+      position = JSON.parse request.body.read
+      block.call(position[0].to_i,position[1].to_i)
       content_type :json
       Oj.dump board
     end
