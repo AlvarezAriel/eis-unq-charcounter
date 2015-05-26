7a196e081130fa6ae2e4151ccbe792746c8b74de
ariel <alvarez.ariel.work@gmail.com>
2015-05-26 18:38:37 -0300

    Refactor de steps

--- batalla2/features/step_definitions/battleship_steps.rb
+++ batalla2/features/step_definitions/battleship_steps.rb
@@ -7,17 +7,17 @@ end
 
 Given(/^(?:I create )?a (large|small) ship in position "([0-9]+):([0-9]+)"$/) do |ship_type, x,y|
   select "Put #{ship_type} ship", from: 'action_selector'
-  find("\#cell-#{x}-#{y}").click
+  get_cell(x, y).click
 end
 
 Then(/^position "([0-9]+):([0-9]+)" is not empty$/) do |x,y|
-  find("\#cell-#{x}-#{y}")[:class].include?('ship').should be true
+  get_cell(x, y)[:class].include?('ship').should be true
 end
 
 Given(/^I shoot to position "([0-9]+):([0-9]+)"$/) do |x,y|
   @shoot_position = [x,y]
   select 'Shoot', from: 'action_selector'
-  find("\#cell-#{x}-#{y}").click
+  get_cell(x, y).click
 end
 
 Then(/^I get (hit|sink)$/) do |status|
@@ -28,12 +28,16 @@ Then(/^I get water$/) do
   cell_includes_class 'ship' .should be false
 end
 
-def cell_includes_class(css_class)
-  find("\#cell-#{@shoot_position[0]}-#{@shoot_position[1]}")[:class].include?(css_class)
-end
-
 Then(/^an error should be thrown$/) do
   @error.should_not be_nil
+end
+
+def get_cell(x, y)
+  find("\#cell-#{x}-#{y}")
+end
+
+def cell_includes_class(css_class)
+  get_cell(@shoot_position[0], @shoot_position[1])[:class].include?(css_class)
 end
 
 Transform /^(-?\d+)$/ do |number|
